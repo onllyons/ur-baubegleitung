3a67bd5d984399f2f02cb4d799aad3dee74e5867
Sergiu <condratiuc.sergiu@gmail.com>
2026-01-29 16:22:17 +0100

    1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <style>
+            body {
+                margin: 0;
+                background: #ffffff;
+                color: #000000;
+                font-family: Arial, sans-serif;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                min-height: 100vh;
+            }
+        </style>
+    </head>
+    <body>
+        <div>test heelo</div>
+    </body>
+</html>
